757a28fcf5e10ab1fbbc99ac334bd8dc87fe694a
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-03-26 19:56:39 +0100

    Game fields as enum

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ApplicationRecord
   belongs_to :user
+
+  MOVEMENTS = { rock: 0, paper: 1, scissors: 2 }
+  RESULTS = { win: 0, lose: 1, draw: 2}
+  
+  enum user_movement: MOVEMENTS, _prefix: :user
+  enum bot_movement: MOVEMENTS, _prefix: :bot
+
+  enum winner: RESULTS
 end
